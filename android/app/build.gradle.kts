03e08ddcd281744f3841659038574ccaf4e4383b
ALENOC <legal@raventag.com>
2026-03-26 20:37:40 +0100

    Fix: Show 'Unable to Verify' when server doesn't respond
    - Added new string verifyUnableToVerify in all 9 languages
    - Updated VerifyScreen logic to use verifyUnableToVerify when error is present
    - This prevents showing 'Not Authentic' when the issue is just a server timeout
    - Bump version to 1.0.2 (versionCode 3)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "io.raventag.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
